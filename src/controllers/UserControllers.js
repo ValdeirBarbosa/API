daea107e655b31412f04f1708cd1ec1c275fba58
valdeirbarbosa <valdeir.barbosa@outlook.com>
2023-05-26 16:07:38 -0300

    install knex

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -12,7 +12,7 @@ class UserControllers {
     if (checkUserExists) {
       throw new AppError(`The email ${email} is already in use.`)
     }
-    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?) ', [name, email, password])
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?) ', [name, email, hashedPassword])
     return response.status(201).json({})
   }
 
@@ -27,36 +27,38 @@ class UserControllers {
       throw new AppError("User not found!")
     }
     const userWithUpdatedMail = await database.get("SELECT  * FROM users WHERE email = (?)", [email])
-    
+
     if (userWithUpdatedMail && userWithUpdatedMail.id !== user.id) {
       throw new AppError(`The email ${userWithUpdatedMail.mail} is already in use.`)
     }
-    user.name = name;
-    user.email = email;
+    //nullish operator
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
-    if(password && !old_password){
+    if (password && !old_password) {
       console.log(user.password);
-      throw new AppError('The old password is required to update the password! ')
+      throw new AppError('The old password is required to update the password!')
     }
 
-    if (password && old_password){
-      const checkOldPassword = await compare(old_password,user.password)
-      if(!checkOldPassword){
-        throw new AppError("The old password didn't match with values on database")
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError(`The old password  didn't match with values on database----`)
       }
       user.password = await hash(password, 8)
     }
 
-    
+
     await database.run(
-        `UPDATE users SET 
+      `UPDATE users SET 
         name = ?,  
         email = ?, 
         password = ?,
-        updated_at = DATETIME('now', localtime') 
-        WHERE id = ?`, [user.name, user.email, user.password,user.id]
+        updated_at = DATETIME('now') 
+        WHERE id = ?`, [user.name, user.email, user.password, user.id]
     );
-    return response.status(201).json({message:"Updated"})
+    return response.status(201).json({ message: "Updated" })
   }
 
 }
